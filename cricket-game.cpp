c3b97410740aa337e81d7527a6eb1c4f5adda04b
Mahreen-Asama <79749919+Mahreen-Asama@users.noreply.github.com>
2021-05-27 16:13:37 +0500

    Update cricket-game.cpp

--- cricket-game.cpp
+++ cricket-game.cpp
@@ -7,13 +7,13 @@ int CPUballing(string);
 int CPUbatting(string);
 int Toss();
 void Decision(int,int);
-const double pitchlength = 22.56;    //standard pitch length is about 22 meters  
+const double pitchlength = 22.56;    	//standard pitch length is about 22 meters  
 const double distanceV = 2.44;		//ball of length (starting from pitch) above 2.44-meters will be considered as no ball...
-const double distanceH = 1;			//ball of width above 1m will be considered as wide ball...
-const double height = 1.3;          //ball of height above 1.3m will be considered as bouncer...
+const double distanceH = 1;		//ball of width above 1m will be considered as wide ball...
+const double height = 1.3;       	//ball of height above 1.3m will be considered as bouncer...
 const double lowangle = -5;		//ball of angle deviation less than -5Degrees(from throwing angle of ball)......OR
-const double highangle = 5;        //ball of angle deviation more than 5Degrees(from throwing angle of ball) ....is Spin ball...
-						//" OUT will be only in the case of Spin Ball if angle crosses 10Degrees deviation (more or less) ".....
+const double highangle = 5;       	//ball of angle deviation more than 5Degrees(from throwing angle of ball) ....is Spin ball...
+					//" OUT will be only in the case of Spin Ball if angle crosses 10Degrees deviation (more or less) ".....
 double entereddistanceV = 0;
 double entereddistanceH = 0;
 double enteredheight = 0;
@@ -112,8 +112,6 @@ int CPUballing(string fs)
 		ballsrem--;
 		cout << "Runs: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
 
-
-
 		//2nd ball, wide ball ..
 	    entereddistanceH = 1.3; 
 		cout << "\nIt is wide ball:\nenter 1 if you want to strike ,,,0 if not: "; cin >> boo;
@@ -121,8 +119,6 @@ int CPUballing(string fs)
 		else            runs = 1;			//1 extra score for wide ball...ball not counts..
 		truns = truns + runs;
 		cout << "Runs: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
-
-
 
 		//3rd ball ...maaking it bouncer...
 		enteredheight = 2;       
@@ -132,8 +128,6 @@ int CPUballing(string fs)
 		truns = truns + runs;
 		ballsrem--;
 		cout << "Runs: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
-
-
 
 		//fourth ball...making it spin...
 		enteredangle = 12;          //angle deviation is of 12-Degrees from throwing angle of ball...
@@ -170,7 +164,6 @@ int CPUbatting(string ss)
 		cout << "width entered above 1m will bs considered as wide ball: "; cin >> entereddistanceH;
 		cout << "height entered above 1.3m will be considered as bouncer: "; cin >> enteredheight;
 		cout << "angle deviation more than 5 or -5 Degrees (from throwing angle of ball) will give spin ball: "; cin >> enteredangle;
-		
 		if (entereddistanceV > distanceV)
 		{
 			cout << "\nIt was no ball, I have played...";  //CPU wants to strike
@@ -179,7 +172,6 @@ int CPUbatting(string ss)
 			truns = truns + runs;
 			cout << "\nRuns: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
 		}
-
 		else if (entereddistanceH > distanceH)
 		{
 			cout << "\nIt was wide ball, I have played...";			//CPU wants to strike
@@ -187,7 +179,6 @@ int CPUbatting(string ss)
 			truns = truns + runs;
 			cout << "\nRuns: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
 		}
-		
 		else if (enteredheight > height)
 		{
 			cout << "\nIt was bouncer ball, I have played...";			//CPU don't wants to strike
@@ -195,7 +186,6 @@ int CPUbatting(string ss)
 			ballsrem--;
 			cout << "\nRuns: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
 		}
-
 		else if (enteredangle > highangle || enteredangle <lowangle)
 		{
 			cout << "\nIt was spin ball, I have played...";				//CPU wants to strike
@@ -212,12 +202,10 @@ int CPUbatting(string ss)
 			ballsrem--;
 			cout << "\nRuns: " << truns << "\nRemaining Balls: " << ballsrem << "\nRemaining Wickets: " << wicketsremain << endl;
 		}
-
 		else
 		{
 			cout << "enter properly\n";
 		}
-		
 	}
 	return truns;
 }
